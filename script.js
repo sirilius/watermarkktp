0604c69f0cf041b37293da68011d8299b518fe91
Sirilius Kevin <siriliuskevin@gmail.com>
2021-10-08 14:09:42 +0700

    Membuat teks pada canvas menjadi dragable

--- script.js
+++ script.js
@@ -1,178 +1,168 @@
 var val = "";
 var inputFile = document.getElementById("inputFile");
     inputFile.addEventListener("change", handleImage, false)
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 
-function automate(img) {
+var $canvas = $("#canvas");
+var canvasOffset = $canvas.offset();
+var offsetX = canvasOffset.left;
+var offsetY = canvasOffset.top;
+var scrollX = $canvas.scrollLeft();
+var scrollY = $canvas.scrollTop();
 
-    document.getElementById("text").addEventListener("keyup", function() {
-        if (document.getElementById("top-left").checked) {
-            var align = "start";
-            var vertical = "40";
-            var horizontal = "30";
-        }
+var startX;
+var startY;
+var texts = [];
 
-        if (document.getElementById("top-center").checked) {
-            var align = "center";
-            var vertical = "40";
-            var horizontal = "400";
-        }
+var selectedText = -1;
 
-        if (document.getElementById("top-right").checked) {
-            var align = "end";
-            var vertical = "40";
-            var horizontal = "770";
-        }
-
-        if (document.getElementById("bottom-left").checked) {
-            var align = "start";
-            var vertical = "530";
-            var horizontal = "30";
-        }
-
-        if (document.getElementById("bottom-center").checked) {
-            var align = "center";
-            var vertical = "530";
-            var horizontal = "400";
-        }
-
-        if (document.getElementById("bottom-right").checked) {
-            var align = "end";
-            var vertical = "530";
-            var horizontal = "770";
-        }
-
-        var color = document.getElementById("colorPicker").value;
-
+function draw() {
+    for (var i = 0; i < texts.length; i++) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        theimg(img);
-        thetext();
-        val = document.getElementById("text").value;
-        ctx.font = "30px Arial";
-        ctx.textAlign = align;
-        ctx.fillStyle = color;
-        ctx.fillText(val, horizontal, vertical);
-});
-
-document.addEventListener("click", function() {
-    if (document.getElementById("top-left").checked) {
-        align = "start";
-        vertical = "40";
-        horizontal = "30";
+        var text = texts[i];
+        ctx.fillText(text.text, text.x, text.y);
     }
-
-    if (document.getElementById("top-center").checked) {
-        var align = "center";
-        var vertical = "40";
-        var horizontal = "400";
-    }
-
-    if (document.getElementById("top-right").checked) {
-        var align = "end";
-        var vertical = "40";
-        var horizontal = "770";
-    }
-
-    if (document.getElementById("bottom-left").checked) {
-        var align = "start";
-        var vertical = "530";
-        var horizontal = "30";
-    }
-
-    if (document.getElementById("bottom-center").checked) {
-        var align = "center";
-        var vertical = "530";
-        var horizontal = "400";
-    }
-
-    if (document.getElementById("bottom-right").checked) {
-        var align = "end";
-        var vertical = "530";
-        var horizontal = "770";
-    }
-
-    var color = document.getElementById("colorPicker").value;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    theimg(img);
-    thetext();
-    val = document.getElementById("text").value;
-    ctx.font = "30px Arial";
-    ctx.textAlign = align;
-    ctx.fillStyle = color;
-    ctx.fillText(val, horizontal, vertical);
-});
-
-document.getElementById("colorPicker").addEventListener("change", function() {
-    if (document.getElementById("top-left").checked) {
-        align = "start";
-        vertical = "40";
-        horizontal = "30";
-    }
-
-    if (document.getElementById("top-center").checked) {
-        var align = "center";
-        var vertical = "40";
-        var horizontal = "400";
-    }
-
-    if (document.getElementById("top-right").checked) {
-        var align = "end";
-        var vertical = "40";
-        var horizontal = "770";
-    }
-
-    if (document.getElementById("bottom-left").checked) {
-        var align = "start";
-        var vertical = "530";
-        var horizontal = "30";
-    }
-
-    if (document.getElementById("bottom-center").checked) {
-        var align = "center";
-        var vertical = "530";
-        var horizontal = "400";
-    }
-
-    if (document.getElementById("bottom-right").checked) {
-        var align = "end";
-        var vertical = "530";
-        var horizontal = "770";
-    }
-
-    var color = document.getElementById("colorPicker").value;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    theimg(img);
-    thetext();
-    val = document.getElementById("text").value;
-    ctx.font = "30px Arial";
-    ctx.textAlign = align;
-    ctx.fillStyle = color;
-    ctx.fillText(val, horizontal, vertical);
-    });
-};
-        
-function thetext() {
-    ctx.fillStyle = "white";
-    ctx.font = "0px Arial";
-    ctx.textAlign = "center";
-    ctx.fillText(val, 0, 0)
 }
 
-function theimg(img) {
-    var canvas = ctx.canvas ;
-    var hRatio = canvas.width / img.width;
-    var vRatio = canvas.height / img.height;
-    var ratio = Math.min(hRatio, vRatio);
-    var centerShift_x = (canvas.width - img.width*ratio) / 2;
-    var centerShift_y = (canvas.height - img.height*ratio) / 2;
+function textHittest(x, y, textIndex) {
+    var text = texts[textIndex];
+    return (x >= text.x && x <= text.x + text.width && y >= text.y - text.height && y <= text.y);
+}
 
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width*ratio, img.height*ratio);
-    ctx.fillStyle = '#ffffff00';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+function handleMouseDown(e) {
+    e.preventDefault();
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+    // Put your mousedown stuff here
+    for (var i = 0; i < texts.length; i++) {
+        if (textHittest(startX, startY, i)) {
+            selectedText = i;
+        }
+    }
+}
+
+function handleMouseUp(e) {
+    e.preventDefault();
+    selectedText = -1;
+}
+
+function handleMouseOut(e) {
+    e.preventDefault();
+    selectedText = -1;
+}
+
+function handleMouseMove(i, img) {
+    if (selectedText < 0) {
+        return;
+    }
+    i.preventDefault();
+    mouseX = parseInt(i.clientX - offsetX);
+    mouseY = parseInt(i.clientY - offsetY);
+
+    var dx = mouseX - startX;
+    var dy = mouseY - startY;
+    startX = mouseX;
+    startY = mouseY;
+
+    var text = texts[selectedText];
+    text.x += dx;
+    text.y += dy;
+    theimg(img);
+}
+
+$("#canvas").mousedown(function (e) {
+    handleMouseDown(e);
+});
+
+$("#canvas").mouseup(function (e) {
+    handleMouseUp(e);
+});
+
+$("#canvas").mouseout(function (e) {
+    handleMouseOut(e);
+});
+
+function automate(img) {
+    document.getElementById("text").addEventListener("keyup", function () {
+    var y = 90;
+    var x = 400;
+    var color = document.getElementById("colorPicker").value
+
+    for (var i = 0; i < texts.length; i++) {
+        var text = texts[i];
+        var y = text.y;
+        var x = text.x;
+    }
+
+    var text = {
+        text: $("#text").val(),
+        x: x,
+        y: y
+    };
+
+    ctx.font = "30px verdana";
+    ctx.fillStyle = color;
+    ctx.textAlign = "center";
+    text.width = ctx.measureText(text.text).width;
+    text.height = 30;
+
+    texts.push(text);
+
+    theimg(img)
+    })
+
+    $("#canvas").mousemove(function (i) {
+        handleMouseMove(i, img)
+    })
+
+    document.getElementById("colorPicker").addEventListener("change", function () {
+        var y = 90;
+        var x = 400;
+        var color = document.getElementById("colorPicker").value
+        
+        for (var i = 0; i < texts.length; i++) {
+            var text = texts[i];
+            var y = text.y;
+            var x = text.x;
+        }
+
+        var text = {
+            text: $("#text").val(),
+            x: x,
+            y: y
+        };
+
+        ctx.font = "30px verdana";
+        ctx.fillStyle = color;
+        ctx.textAlign = "center";
+        text.width = ctx.measureText(text.text).width;
+        text.height = 30;
+
+        texts.push(text);
+
+        theimg(img)
+        })
+    
+        $("#canvas").mousemove(function (i) {
+            handleMouseMove(i, img)
+        })
+};
+
+function theimg(img) {
+    for (var i = 0; i < texts.length; i++) {
+        var acanvas = ctx.canvas ;
+        var hRatio = acanvas.width / img.width;
+        var vRatio = acanvas.height / img.height;
+        var ratio = Math.min(hRatio, vRatio);
+        var centerShift_x = (acanvas.width - img.width*ratio) / 2;
+        var centerShift_y = (acanvas.height - img.height*ratio) / 2;
+            
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width*ratio, img.height*ratio);
+        var text = texts[i];
+        ctx.fillText(text.text, text.x, text.y);
+    }
 }
 
 function handleImage(e) {
@@ -200,7 +190,6 @@ function handleImage(e) {
         src= event.target.result;
         canvas.classList.add("show");
         theimg(img);
-        thetext();
         automate(img);
     }
 
@@ -209,11 +198,11 @@ function handleImage(e) {
 
 function download() {
     var download = document.getElementById("download");
-    var image = document.getElementById("myCanvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
+    var image = document.getElementById("canvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
 }
 
-$("#inputFile").on("change", function() {
+$("#inputFile").change(function() {
     var filename = $(this).val().split("\\").pop();
     $(this).siblings(".label-file").html(filename);
 
