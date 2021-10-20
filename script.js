d15aa49da49c8627ea1598c9948fcc195d626ec3
Sirilius Kevin <19952075+sirilius@users.noreply.github.com>
2021-10-20 11:22:32 +0700

    Add close button function

--- script.js
+++ script.js
@@ -330,3 +330,7 @@ window.onclick = function(event) {
 document.getElementsByClassName("nav-bar")[0].onclick = function () {
   document.getElementById("pop-up").style.display = "flex";
 }
+
+document.getElementsByClassName("close-btn")[0].onclick = function () {
+  document.getElementById("pop-up").style.display = "none";
+}
