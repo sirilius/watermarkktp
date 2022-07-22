39bb47c8dee66cdef5065645a1bc5f954a8e823b
Sirilius Kevin <19952075+sirilius@users.noreply.github.com>
2022-07-23 00:04:52 +0700

    migrate to Tailwind

--- src/_includes/js/mobile-menu.js
+++ src/_includes/js/mobile-menu.js
@@ -1,13 +1,18 @@
-window.onclick = function (event) {
-  if (event.target == document.getElementById("pop-up")) {
-    document.getElementById("pop-up").style.display = "none";
+var menu = document.getElementById("menu");
+var line_1 = document.getElementById("line-1");
+var line_2 = document.getElementById("line-2");
+var line_3 = document.getElementById("line-3");
+
+function toggle_menu() {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    line_1.style.transform = "translate3d(0px, 5.3px, 0px) rotate(45deg)";
+    line_2.classList.add("hidden");
+    line_3.style.transform = "translate3d(0px, -5.3px, 0px) rotate(-45deg)";
+  } else {
+    menu.classList.add("hidden");
+    line_1.style.transform = "translate3d(0px, 0px, 0px) rotate(0deg)";
+    line_2.classList.remove("hidden");
+    line_3.style.transform = "translate3d(0px, 0px, 0px) rotate(0deg)";
   }
-};
-
-document.getElementsByClassName("nav-bar")[0].onclick = function () {
-  document.getElementById("pop-up").style.display = "flex";
-};
-
-document.getElementsByClassName("close-btn")[0].onclick = function () {
-  document.getElementById("pop-up").style.display = "none";
-};
+}
