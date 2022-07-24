c5ad05552965825d98350fbd8db8b37f5fdb764e
Sirilius Kevin <19952075+sirilius@users.noreply.github.com>
2022-07-24 18:41:44 +0700

    adding js to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,njk}"],
+  content: ["./src/**/*.{html,njk,js}"],
   theme: {
     extend: {
       height: {
