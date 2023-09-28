8f808348616c3be053e8d18b3972306c03df667f
Frans Allen <fransallen@users.noreply.github.com>
2023-09-29 00:58:10 +0700

    include _redirects in outputdir

--- .eleventy.js
+++ .eleventy.js
@@ -46,6 +46,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy("_headers");
+  eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy({
     "src/_includes/css/app.css": "./css/app.css",
