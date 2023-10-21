84de7ef3988f3682e72da9ac55f6043401408d62
sirilius <19952075+sirilius@users.noreply.github.com>
2023-10-21 22:06:41 +0700

    add faqs component

---
+++ src/_data/faqs.js
@@ -0,0 +1,29 @@
+const faqs = [
+  {
+    question: "Apa itu Watermark KTP?",
+    answer:
+      'Watermark KTP adalah sebuah alat sederhana untuk menambahkan watermark pada scan KTP. Watermark yang ditambahkan berupa teks yang berisi tanggal dan untuk apa scan KTP tersebut digunakan. Contoh "Verifikasi e-wallet, 10-10-2021".',
+  },
+  {
+    question: "Apa itu watermark?",
+    answer:
+      "Watermark adalah gambar atau teks yang biasanya ditempatkan pada foto atau dokumen digital untuk menunjukkan kepemilikan atau asal-usulnya. Watermark ini seringkali transparan dan diletakkan di sudut gambar agar tidak mengganggu kontennya. Tujuannya adalah untuk melindungi hak cipta, mengidentifikasi pencipta, atau memberikan informasi tentang sumber gambar tersebut.",
+  },
+  {
+    question: "Apakah Watermark KTP diperlukan?",
+    answer:
+      "Watermark KTP diperlukan karena merupakan langkah penting dalam     menjaga keamanan scan KTP. Dengan menambahkan watermark berisi      informasi seperti tanggal dan tujuan pemakaian, kita dapat membantu      mencegah penyalahgunaan scan KTP tersebut. Watermark ini membuatnya      lebih sulit bagi orang lain untuk menggunakan scan KTP dengan cara      yang tidak sah atau merugikan.",
+  },
+  {
+    question: "Apakah Watermark KTP aman?",
+    answer:
+      'Semua proses yang dilakukan di alat Watermark KTP 100% dilakukan di      sisi klien (sisi Anda), kami tidak pernah mengirimkan berkas yang      ditaruh Pengguna keluar dari peramban Pengguna. Untuk informasi lebih lanjut, dapat anda baca di <a class="font-medium text-black" href="/privasi/">halaman privasi</a>.',
+  },
+  {
+    question: "Bagaimana cara memakai Watermark KTP?",
+    answer:
+      'Anda dapat membaca panduan penggunaan Watermark KTP di halaman <a class="font-medium text-black" href="/cara-pakai/">Cara Pakai</a>.',
+  },
+];
+
+module.exports = faqs;
