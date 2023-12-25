f1c6844772509232de9573f55eaab9ddcfb649f2
sirilius <19952075+sirilius@users.noreply.github.com>
2023-12-25 23:33:51 +0700

    Add testimonial carousel

---
+++ src/_data/testimonialsSlide.js
@@ -0,0 +1,31 @@
+const testimonialsSlide = [
+  {
+    name: "Teguh Aprianto",
+    umami: "teguhaprianto",
+    image: "testi-pp-@secgron.jpg",
+    profession: "Cybersecurity Consultant",
+    url: "https://x.com/secgron/status/1447231684139573255",
+    content:
+      'Buat yg nanya aman atau engga, <span class="bg-violet-600 text-white py-0.5 px-1">semua proses dilakukan dari sisi client.</span> Artinya tidak ada perpindahan file yg kamu upload ke sisi server.',
+  },
+  {
+    name: "Frans Allen",
+    umami: "fransallen",
+    image: "testi-pp-@fransallen.jpg",
+    profession: "Software Engineer",
+    url: "https://x.com/fransallen/status/1546407297681063936",
+    content:
+      'Watermark KTP adalah solusi berbasis web yang <span class="bg-violet-600 text-white py-0.5 px-1">aman dan praktis</span> untuk memberi penandaan pada hasil scan KTP.',
+  },
+  {
+    name: "Arsenal's Kitchen",
+    umami: "arsenalskitchen",
+    image: "testi-pp-@arsenalskitchen.jpg",
+    profession: "Netizen",
+    url: "https://x.com/arsenalskitchen/status/1546643994716778496",
+    content:
+      'Watermark KTP memang <span class="bg-violet-600 text-white py-0.5 px-1">sangat berguna,</span> salah satunya untuk <span class="bg-violet-600 text-white py-0.5 px-1">memerangi pinjol laknat.</span>',
+  },
+];
+
+module.exports = testimonialsSlide;
