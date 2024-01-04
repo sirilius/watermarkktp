cd7f6c8a2baa6abd4cedfb9e362346b56ff0ba39
sirilius <19952075+sirilius@users.noreply.github.com>
2024-01-04 10:16:36 +0700

    Add share button for article

---
+++ src/_data/share.js
@@ -0,0 +1,24 @@
+const share = [
+  {
+    name: "linkedin",
+    bg: "bg-[#0077B5]",
+    icon: "ri-linkedin-fill",
+  },
+  {
+    name: "twitter",
+    bg: "bg-black",
+    icon: "ri-twitter-x-line",
+  },
+  {
+    name: "facebook",
+    bg: "bg-[#3b5998]",
+    icon: "ri-facebook-fill",
+  },
+  {
+    name: "whatsapp",
+    bg: "bg-[#25D366]",
+    icon: "ri-whatsapp-line",
+  },
+];
+
+module.exports = share;
