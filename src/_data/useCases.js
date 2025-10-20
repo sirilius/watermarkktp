82b581be8e17bc919deb99c9eaca639dd08e44f7
sirilius <19952075+sirilius@users.noreply.github.com>
2025-10-20 23:14:22 +0700

    add use case section

---
+++ src/_data/useCases.js
@@ -0,0 +1,34 @@
+const useCases = [
+  {
+    icon: "car-line",
+    title: "SIM",
+    desc: "Lindungi SIM dari penyalahgunaan saat upload persyaratan online",
+  },
+  {
+    icon: "id-card-line",
+    title: "KTP",
+    desc: "Amankan KTP dengan watermark untuk verifikasi identitas",
+  },
+  {
+    icon: "team-line",
+    title: "Kartu Keluarga",
+    desc: "Tambahkan watermark pada KK untuk keperluan administrasi",
+  },
+  {
+    icon: "book-line",
+    title: "Paspor",
+    desc: "Proteksi data paspor saat dikirim via email atau aplikasi",
+  },
+  {
+    icon: "briefcase-3-line",
+    title: "NPWP",
+    desc: "Watermark NPWP untuk transaksi dan registrasi bisnis",
+  },
+  {
+    icon: "file-shield-2-line",
+    title: "Dokumen Lain",
+    desc: "Semua jenis dokumen identitas pribadi lainnya",
+  },
+];
+
+module.exports = useCases;
