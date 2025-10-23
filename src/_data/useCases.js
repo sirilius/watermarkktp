75d71731482ee976981897e2de24f4596ec301de
sirilius <19952075+sirilius@users.noreply.github.com>
2025-10-23 22:45:34 +0700

    Update use cases with URLs and move use case section below  testimonials

--- src/_data/useCases.js
+++ src/_data/useCases.js
@@ -1,33 +1,39 @@
 const useCases = [
   {
-    icon: "car-line",
-    title: "SIM",
-    desc: "Lindungi SIM dari penyalahgunaan saat upload persyaratan online",
-  },
-  {
     icon: "id-card-line",
     title: "KTP",
     desc: "Amankan KTP dengan watermark untuk verifikasi identitas",
+    url: "https://watermarkktp.com/blog/cara-mengamankan-scan-ktp-dengan-watermark-ktp/",
+  },
+  {
+    icon: "car-line",
+    title: "SIM",
+    desc: "Lindungi SIM dari penyalahgunaan saat upload persyaratan online",
+    url: "https://watermarkktp.com/blog/kenapa-foto-sim-perlu-watermark/",
   },
   {
     icon: "team-line",
     title: "Kartu Keluarga",
     desc: "Tambahkan watermark pada KK untuk keperluan administrasi",
+    url: "https://watermarkktp.com/blog/cara-menambahkan-watermark-kartu-keluarga/",
   },
   {
     icon: "book-line",
     title: "Paspor",
     desc: "Proteksi data paspor saat dikirim via email atau aplikasi",
+    url: "https://watermarkktp.com/blog/lindungi-data-paspor-dengan-watermark/",
   },
   {
     icon: "briefcase-3-line",
     title: "NPWP",
     desc: "Watermark NPWP untuk transaksi dan registrasi bisnis",
+    url: "https://watermarkktp.com/",
   },
   {
     icon: "file-shield-2-line",
     title: "Dokumen Lain",
     desc: "Semua jenis dokumen identitas pribadi lainnya",
+    url: "https://watermarkktp.com/",
   },
 ];
 
